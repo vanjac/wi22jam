a09c90294b7a409bb8eb0b51c30f4d8013513d5f
j v <vantjac@gmail.com>
2022-02-06 22:39:39 -0800

    animal state machine

--- enemies/Animal.gd
+++ enemies/Animal.gd
@@ -2,7 +2,9 @@ extends "res://core/Character.gd"
 
 const walk_speed = 100
 const walk_decel = 200
+const wake_time = 0.4
 const recoil_time = 0.4
+const chase_time = 10
 const step_time = 0.2
 const knockback_speed = 150
 const knockback_decel = 200
@@ -10,8 +12,10 @@ const knockback_decel = 200
 var sprite
 var player
 
-var angry = false
-var recoil = 0
+enum State { TAME, WAKE, CHASE, RECOIL }
+var state = State.TAME
+var time_in_state = 0
+
 var step = 0
 
 func _ready():
@@ -19,19 +23,30 @@ func _ready():
 	player = get_tree().get_root().find_node("Player", true, false)
 
 func _process(delta):
-	if angry:
-		step += delta
-		if step > step_time:
-			step -= step_time
-			sprite.frame = (sprite.frame + 1) % 2
+	time_in_state += delta
+
+	match state:
+		State.WAKE:
+			if time_in_state >= wake_time:
+				set_state(State.CHASE)
+		State.CHASE:
+			step += delta
+			if step > step_time:
+				step -= step_time
+				sprite.frame = (sprite.frame + 1) % 2
+			if time_in_state >= chase_time:
+				set_state(State.TAME)
+		State.RECOIL:
+			if time_in_state >= recoil_time:
+				set_state(State.CHASE)
 
 func _physics_process(delta):
-	if angry and is_instance_valid(player):
-		var to_player = player.position.x - position.x
-		if recoil > 0:
-			move_vec.x = -sign(to_player) * walk_speed * (recoil / recoil_time)
-			recoil -= delta
-		elif abs(to_player) >= sprite.get_rect().size.x / 2:
+	var to_player = 0
+	if is_instance_valid(player):
+		to_player = player.position.x - position.x
+
+	if state == State.CHASE:
+		if abs(to_player) >= sprite.get_rect().size.x / 2:
 			move_vec.x = sign(to_player) * walk_speed
 		else:
 			move_vec.x = decelerate(move_vec.x, walk_decel * delta)
@@ -41,20 +56,26 @@ func _physics_process(delta):
 
 	move_character(delta)
 
-	if angry:
+	if state == State.CHASE:
 		for i in get_slide_count():
 			var collision = get_slide_collision(i)
 			if collision.collider.has_method("_on_attacked"):
 				collision.collider._on_attacked()
-				recoil = recoil_time
+				set_state(State.RECOIL)
+				move_vec.x = -sign(to_player) * walk_speed
 
 func _on_angered():
-	angry = true
+	set_state(State.WAKE)
 	collision_layer = 1  # move to foreground
 	sprite.frame = 0
 
 func _on_shot(position, direction):
-	angry = false
+	set_state(State.TAME)
 	collision_layer = 4  # anger
 	sprite.frame = 2
 	move_vec.x = direction.x * knockback_speed
+
+
+func set_state(s):
+	state = s
+	time_in_state = 0
