bde4fdbbeae058021319c1504eb1f7db1a6662d3
j v <vantjac@gmail.com>
2022-02-06 19:43:08 -0800

    walking animation

--- enemies/Animal.gd
+++ enemies/Animal.gd
@@ -1,24 +1,38 @@
 extends "res://core/Character.gd"
 
-const walk_speed = 150
+const walk_speed = 100
 const recoil_time = 0.4
+const step_time = 0.2
 
+var sprite
 var player
 
 var angry = false
 var recoil = 0
+var step = 0
 
 func _ready():
+	sprite = $Sprite
 	player = get_tree().get_root().find_node("Player", true, false)
+
+func _process(delta):
+	if angry:
+		step += delta
+		if step > step_time:
+			step -= step_time
+			sprite.frame = (sprite.frame + 1) % 2
 
 func _physics_process(delta):
 	if angry and is_instance_valid(player):
-		var to_player = sign(player.position.x - position.x)
+		var to_player = player.position.x - position.x
 		if recoil > 0:
-			move_vec.x = -to_player * walk_speed * (recoil / recoil_time)
+			move_vec.x = -sign(to_player) * walk_speed * (recoil / recoil_time)
 			recoil -= delta
+		elif abs(to_player) >= sprite.get_rect().size.x / 2:
+			move_vec.x = sign(to_player) * walk_speed
 		else:
-			move_vec.x = to_player * walk_speed
+			move_vec.x = 0
+		sprite.scale.x = -1 if to_player >= 0 else 1
 	move_character(delta)
 	if angry:
 		for i in get_slide_count():
@@ -30,4 +44,4 @@ func _physics_process(delta):
 func _on_shot():
 	angry = true
 	collision_layer = 1  # move to foreground
-	$Sprite.modulate = Color(1, 0, 0)  # red
+	sprite.frame = 0
