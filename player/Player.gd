819bfc3089a5566d9c9762f9bc882b1a376174f1
j v <vantjac@gmail.com>
2022-02-07 21:04:48 -0800

    Use player sprite

--- player/Player.gd
+++ player/Player.gd
@@ -10,17 +10,22 @@ var can_dash = true
 var dash_direction : Vector2
 
 onready var dash_timer = $dash_timer
+onready var sprite = $Sprite
 
-const walk_speed = 250
+const walk_speed = 200
 const walk_accel = 5000
 const walk_decel = 1000
 const jump_speed = 400
 const coyote_time = 0.1
 const knockback_speed = 175
+const walk_step_time = 0.15
+const stand_step_time = 0.3
+const dash_step_time = 0.07
 
 var walk = 0  # 0, 1, or -1
 var jumping = false
 var time_since_left_ground = 0
+var step = 0
 
 var health = 4
 
@@ -50,6 +55,24 @@ func _process(delta):
 		jumping = true
 		on_ground = false
 
+	# update animation
+
+	step += delta
+	if walk != 0:
+		sprite.scale.x = walk
+	if is_dashing:
+		anim_step(dash_step_time, 6, 7)
+	elif not on_ground:
+		if move_vec.y < 0:
+			sprite.frame = 5
+		else:
+			sprite.frame = 4
+	else:
+		if walk != 0:
+			anim_step(walk_step_time, 2, 3)
+		else:
+			anim_step(stand_step_time, 0, 1)
+
 func _physics_process(delta):
 	if on_ground:
 		time_since_left_ground = 0
@@ -68,7 +91,7 @@ func _physics_process(delta):
 
 	if (is_dashing):
 		move_vec = dash_direction
-	
+
 	move_character(delta)
 
 func _on_attacked(direction):
@@ -85,25 +108,34 @@ func damage(amt):
 
 func dash_timer_timeout():
 	is_dashing = false
-	
+
 
 func handle_dash():
 	if (Input.is_action_pressed("dash") && can_dash && !on_ground):
 		Engine.time_scale = 0.1
 	else:
 		Engine.time_scale = 1
-	
+
 	if (Input.is_action_just_released("dash") && can_dash && !on_ground):
 		is_dashing = true
 		can_dash = false
-				
+
 		dash_direction = get_local_mouse_position()
 		dash_direction = dash_direction.clamped(dash_distance)
 		dash_direction *= dash_speed
-		
+
 		dash_timer.start(dash_time)
-		
+
 	if (is_dashing):
 		if (on_ground): is_dashing = false
 		if (is_on_wall()): is_dashing = false
 		pass
+
+
+func anim_step(step_time, frame_a, frame_b):
+	if step > step_time:
+		step -= step_time
+		if sprite.frame == frame_a:
+			sprite.frame = frame_b
+		else:
+			sprite.frame = frame_a
